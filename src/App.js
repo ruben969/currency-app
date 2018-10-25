b854223fd3ceef4614784cfeb04a817ca8368436
Ruben Arzoyan <ruben.arzoyan@gmail.com>
2018-10-25 11:40:43 +0400

    cheng state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Select from './components/select/Select';
+import Input from './components/input/Input';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currencys: [
+        { RUB: null },
+        { USD: null },
+        { GBP: null },
+        { EUR: null }
+      ]
+
+    }
+  }
+
+  selectedCurrenty(event){
+
+    event.target.value
+
+  }
+
+  componentDidMount() {
+    const currencys = [];
+    this.state.currencys.forEach(element => {
+      fetch('http://cb.am/latest.json.php?currency=' + Object.keys(element)[0])
+        .then(result => result.json())
+        .then(data => {
+          currencys.push(data);
+          this.setState({
+            currencys
+          });
+        })
+    })
+  }
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>React Calculatoer</h1>
+        <Select />
+        <Input />
       </div>
     );
   }
