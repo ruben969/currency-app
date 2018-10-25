4b020c2ebbc4e2019a56419f8675d730f3e27d59
Ruben Arzoyan <ruben.arzoyan@gmail.com>
2018-10-26 00:06:57 +0400

    input value change in AMD

--- src/App.js
+++ src/App.js
@@ -8,46 +8,80 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currencys: [
-        { RUB: null },
-        { USD: null },
-        { GBP: null },
-        { EUR: null }
-      ]
-
+      currencys: {
+        RUB: null,
+        USD: null,
+        GBP: null,
+        EUR: null
+      },
+      currency: { money: 0 },
+      result: 0
     }
   }
 
-  selectedCurrenty(event){
+  selectedCurrenty = (event) => {
+    let currencyss = {};
+    let typeMony = event.target.value;
+    let monyValue = this.state.currencys[typeMony]
+    currencyss[typeMony] = monyValue;
+    this.setState({
+      currency: currencyss
+    });
 
-    event.target.value
-
+    console.log("esi piti tpvi", this.state.currency);
   }
 
   componentDidMount() {
-    const currencys = [];
-    this.state.currencys.forEach(element => {
-      fetch('http://cb.am/latest.json.php?currency=' + Object.keys(element)[0])
+    let currencys = {};
+    let template = {};
+    for (let key in this.state.currencys) {
+      fetch('http://cb.am/latest.json.php?currency=' + key)
         .then(result => result.json())
         .then(data => {
-          currencys.push(data);
+          console.log(data)
+          currencys[key] = data[key];
+          template.RUB = currencys.RUB;
           this.setState({
             currencys
           });
-        })
+
+        });
+    }
+    this.setState({
+      currency: template
     })
   }
+
+  changeMoney = (event) => {
+    const money = event.target.value;
+    let result=0;
+    console.log(money);
+   for(let key in this.state.currency){
+     result= this.state.currency[key];
+   }
+     result = (money / (+result))+'';
+    console.log(this.state.currency.value);
+    this.setState({
+
+      result: result
+    })
+  }
+
+
 
   render() {
 
     return (
       <div className="App">
         <h1>React Calculatoer</h1>
-        <Select />
-        <Input />
+        <Select choose={this.selectedCurrenty}
+          sum={this.state.currency} />
+        <Input change={this.changeMoney}
+          result={this.state.result} />
       </div>
     );
   }
 }
 
 export default App;
+// 
